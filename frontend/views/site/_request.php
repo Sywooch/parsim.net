9e0f6f62f7063d724080f4be838964a0dcfce7ea
PTimofeev <info@ptimofeev.com>
2017-10-04 17:55:06 +0300

    Update site/index

--- frontend/views/site/_request.php
+++ frontend/views/site/_request.php
@@ -12,8 +12,8 @@
             <div class="row clearfix">
               <div class="column col-md-9 col-sm-12 col-xs-12">
                   <div class="row clearfix">
-                        <?= $form->field($model, 'request_url',['options' => ['class' => 'form-group  col-md-7 col-sm-6 co-xs-12']])->textInput(['class'=>'form-control bordered','placeholder'=>Yii::t('app','Type any website address here')])->label(false); ?>
-                        <?= $form->field($model, 'response_email',['options' => ['class' => 'form-group  col-md-5 col-sm-6 co-xs-12']])->textInput(['class'=>'form-control bordered','placeholder'=>Yii::t('app','Email for answer')])->label(false); ?>
+                        <?= $form->field($model, 'request_url',['options' => ['class' => 'form-group  col-md-7 col-sm-6 co-xs-12 mt-15']])->textInput(['class'=>'form-control bordered','placeholder'=>Yii::t('app','Type any website address here')])->label(false); ?>
+                        <?= $form->field($model, 'response_email',['options' => ['class' => 'form-group  col-md-5 col-sm-6 co-xs-12 mt-15']])->textInput(['class'=>'form-control bordered','placeholder'=>Yii::t('app','Email for answer')])->label(false); ?>
                     </div>
                 </div>
                 <div class="column col-md-3 col-sm-12 col-xs-12">
