7eeb526c035585fb19c26d81256e37ba0ccb8da0
PTimofeev <info@ptimofeev.com>
2017-11-22 19:22:57 +0300

    update index page

--- frontend/views/site/_serviceBlocks.php
+++ frontend/views/site/_serviceBlocks.php
@@ -16,16 +16,6 @@
 <!--Services Title-->
 
 <div class="row clearfix" >
-    <!--Services Block-->
-    <div class="services-block col-md-4 col-sm-6 col-xs-12">
-      <div class="inner-box wow fadeIn" data-wow-duration="1500ms" data-wow-delay="0ms">
-          <div class="icon-box">
-              <span class="icon"><img src="/images/resource/icon-1.png" alt="" /></span>
-            </div>
-            <h3><a href="/site/page?view=about#parsim-net-is-easy">Просто <br>добавьте URL и получите результат</a></h3>
-            <a href="/site/page?view=about#parsim-net-is-easy" class="learn-more"><?= Yii::t('app','Learn More'); ?> <span class="arrow fa fa-long-arrow-right"></span></a>
-        </div>
-    </div>
     
     <!--Services Block-->
     <div class="services-block col-md-4 col-sm-6 col-xs-12">
@@ -33,12 +23,21 @@
           <div class="icon-box">
               <span class="icon"><img src="/images/resource/icon-2.png" alt="" /></span>
             </div>
-            <h3><a href="/site/page?view=about#parsim-net-is-fast">Быстро <br>парсер на заказ бесплатно в течении 30 мин. </a></h3>
-            <a href="/site/page?view=about#parsim-net-is-fast" class="learn-more"><?= Yii::t('app','Learn More'); ?><span class="arrow fa fa-long-arrow-right"></span></a>
+            <h3><a href="/site/page?view=about#parsim-net-is-easy">Просто <br>добавьте URL и получите результат</a></h3>
+            <a href="/site/page?view=about#parsim-net-is-easy" class="learn-more"><?= Yii::t('app','Learn More'); ?><span class="arrow fa fa-long-arrow-right"></span></a>
         </div>
     </div>
     
-    
+    <!--Services Block-->
+    <div class="services-block col-md-4 col-sm-6 col-xs-12">
+      <div class="inner-box wow fadeIn" data-wow-duration="1500ms" data-wow-delay="0ms">
+          <div class="icon-box">
+              <span class="icon"><img src="/images/resource/icon-1.png" alt="" /></span>
+            </div>
+            <h3><a href="/site/page?view=about#parsim-net-is-fast">Быстро <br>парсер на заказ бесплатно в течении 30 мин. </a></h3>
+            <a href="/site/page?view=about#parsim-net-is-fast" class="learn-more"><?= Yii::t('app','Learn More'); ?> <span class="arrow fa fa-long-arrow-right"></span></a>
+        </div>
+    </div>
 
     <!--Services Block-->
     <div class="services-block col-md-4 col-sm-6 col-xs-12" id="services-block">
