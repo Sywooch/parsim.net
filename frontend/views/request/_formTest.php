8c3b164fe54ffc8d98c9ab903c3d930c6a4ad4fd
PTimofeev <info@ptimofeev.com>
2017-11-23 16:06:29 +0300

    add recapcha

--- frontend/views/request/_formTest.php
+++ frontend/views/request/_formTest.php
@@ -18,7 +18,12 @@
                   <div class="row clearfix">
                         <?= $form->field($model, 'request_url',['options' => ['class' => 'form-group  col-md-7 col-sm-6 co-xs-12']])->textInput(['class'=>'form-control bordered','placeholder'=>Yii::t('app','Type any website address here')])->label(false); ?>
                         <?= $form->field($model, 'response_email',['options' => ['class' => 'form-group  col-md-5 col-sm-6 co-xs-12']])->textInput(['class'=>'form-control bordered','placeholder'=>Yii::t('app','Email for answer')])->label(false); ?>
-                        <?= $form->field($model, 'reCaptcha')->widget(\himiklab\yii2\recaptcha\ReCaptcha::className()) ?>
+
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 text-center">
+                            <?= $form->field($model, 'reCaptcha')->widget(\himiklab\yii2\recaptcha\ReCaptcha::className()) ?>
+                        </div>
                     </div>
                 </div>
                 <div class="column col-md-3 col-sm-12 col-xs-12">
