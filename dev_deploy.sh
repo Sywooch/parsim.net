05adb6997cf36645d75dcc3cbda81927e228dc9a
PTimofeev <info@ptimofeev.com>
2017-12-27 14:31:40 +0300

    #

--- dev_deploy.sh
+++ dev_deploy.sh
@@ -7,5 +7,11 @@ git push
 
 echo "connect to remote srv via ssh"
 PRJ_PATH="/srv/www/dev.parsim.net"
-ssh -p 9999 pavel@parsim.net -t "sh $PRJ_PATH/deploy.sh $PRJ_PATH "
+
+spawn ssh -p 9999 pavel@parsim.net -t "sh $PRJ_PATH/deploy.sh $PRJ_PATH "
+expect "pavel@parsim.net's password:"    
+send "Ehdym_99\r"       
+interact 
+
+#ssh -p 9999 pavel@parsim.net -t "sh $PRJ_PATH/deploy.sh $PRJ_PATH "
 
