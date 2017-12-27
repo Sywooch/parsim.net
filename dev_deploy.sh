6a6e4cb9c9ca5abdc4744d7730cce6aa37cd575e
PTimofeev <info@ptimofeev.com>
2017-12-27 14:38:19 +0300

    #

--- dev_deploy.sh
+++ dev_deploy.sh
@@ -9,9 +9,4 @@ echo "connect to remote srv via ssh"
 PRJ_PATH="/srv/www/dev.parsim.net"
 
 ssh -p 9999 pavel@parsim.net -t "sh $PRJ_PATH/deploy.sh $PRJ_PATH "
-expect "pavel@parsim.net's password:"    
-send "Ehdym_99\r"       
-interact 
-
-#ssh -p 9999 pavel@parsim.net -t "sh $PRJ_PATH/deploy.sh $PRJ_PATH "
 
