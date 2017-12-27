bb21821cb2f2680411023839efd152a178fcb60e
PTimofeev <info@ptimofeev.com>
2017-12-27 14:04:48 +0300

    #

--- dev_deploy.sh
+++ dev_deploy.sh
@@ -5,6 +5,6 @@ git add .
 git commit -a -m "#"
 git push
 
-#echo "connect to remote srv via ssh"
-#ssh -p 9999 pavel@parsim.net -t "sh /srv/www/dev.parsim.net/deploy.sh; bash"
+echo "connect to remote srv via ssh"
+ssh -p 9999 pavel@parsim.net -t "sh /srv/www/dev.parsim.net/deploy.sh; bash"
 
