7ea25a866e387689073876a788368d1e2336c049
PTimofeev <info@ptimofeev.com>
2018-01-09 19:25:00 +0300

    #

--- backend/views/site/_salesStatView.php
+++ backend/views/site/_salesStatView.php
@@ -18,6 +18,6 @@
     <span class="text-muted text-size-small">06:28 pm</span>
   </td>
   <td>
-    <h6 class="text-semibold no-margin"><?= $model->amount; ?></h6>
+    <h6 class="text-semibold no-margin"><?= Yii::$app->formatter->asCurrency($model->amount); ?></h6>
   </td>
 </tr>
