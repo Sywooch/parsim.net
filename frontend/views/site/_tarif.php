739554ed57ff1a72c5ce5d39576fb4e19e6adff8
PTimofeev <info@ptimofeev.com>
2018-01-12 16:00:10 +0300

    #

--- frontend/views/site/_tarif.php
+++ frontend/views/site/_tarif.php
@@ -1,36 +1,21 @@
-<?php
-    $promoName=[
-        'free-forever'=>'100% Free Forever',
-        'pay-per-action'=>'dynamic',
-        'pay-per-period'=>'static'
-    ];
-?>
-
-<!--Pricing Column-->
+<!--Pricing Column Two-->
 <div class="pricing-column-two col-md-4 col-sm-6 col-xs-12">
     <div class="inner-box">
         <div class="price-header">
             <div class="content">
-                <div class="plan-title"><span class="theme_color"><?= $promoName[$model->alias]; ?></span></div>
-                <?php if($model->alias=='free-forever'): ?>
-                    <h4>Акция в честь запуска<br/></h4>
-                    <span class="last-offers theme_color">Всего предложений - <?= $model->AvailableQty; ?> шт.</span>
-                <?php elseif($model->alias=='pay-per-period'): ?>
-                    <span class="price"><span class="dollar-sign">₽</span><span class="total-amount"> 300 </span> <sup>.00</sup> <sub>/ месяц</sub></span>
-                <?php elseif($model->alias=='pay-per-action'): ?>
-                    <span class="price"><span class="dollar-sign">₽</span><span class="total-amount"> 0 </span> <sup>.10</sup> <sub>/ обновление</sub></span>
-                    
-                <?php endif; ?>
+                <div class="plan-title">basic</div>
+                <span class="price"><span class="dollar-sign">$</span><span class="total-amount"> 9 </span> <sup>.99</sup> <sub>/ month</sub></span>
             </div>
         </div>
         <div class="lower-content">
             <ul class="spec-list">
-                <?php foreach ($model->options as $key => $option): ?>
-                <li><?= $option->title; ?></li>
-                <?php endforeach; ?>
-                
+                <li>90 Keyword</li>
+                <li class="mark-list">No Time Tracking</li>
+                <li>800 - Man Hour</li>
+                <li>News Letter Available</li>
+                <li>User Dasboard</li>
             </ul>
-            <a href="<?= $model->orderUrl; ?>" class="theme-btn purchase-btn">Хочу этот тариф</a>
+            <a href="#" class="theme-btn purchase-btn">Purchase Now</a>
         </div>
     </div>
 </div>
