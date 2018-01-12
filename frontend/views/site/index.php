d5135955ac8984af4534f50b0db609e47465181e
PTimofeev <info@ptimofeev.com>
2018-01-12 14:20:04 +0300

    #

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -14,7 +14,167 @@ $this->params['keywords']='Парсер, парсер сайтов, парсин
 
 ?>
 
-<?= $this->render('_mainSlider'); ?>
+<!--Main Slider-->
+<section class="main-slider main-slider-one">
+  
+    <div class="rev_slider_wrapper fullwidthbanner-container"  id="rev_slider_486_1_wrapper" data-source="gallery">
+        <div class="rev_slider fullwidthabanner" id="rev_slider_486_1" data-version="5.4.1">
+            <ul>
+              
+                <li data-description="Slide Description" data-easein="default" data-easeout="default" data-fsmasterspeed="1500" data-fsslotamount="7" data-fstransition="fade" data-hideafterloop="0" data-hideslideonmobile="off" data-index="rs-1687" data-masterspeed="default" data-param1="" data-param10="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-rotate="0" data-saveperformance="off" data-slotamount="default" data-thumb="images/main-slider/image-1.jpg" data-title="Slide Title" data-transition="parallaxvertical">
+                <img alt="" class="rev-slidebg" data-bgfit="cover" data-bgparallax="10" data-bgposition="center center" data-bgrepeat="no-repeat" data-no-retina="" src="/images/main-slider/image-1.jpg"> 
+                
+                <div class="tp-caption tp-resizeme" 
+                data-paddingbottom="[0,0,0,0]"
+                data-paddingleft="[0,0,0,0]"
+                data-paddingright="[0,0,0,0]"
+                data-paddingtop="[0,0,0,0]"
+                data-responsive_offset="on"
+                data-type="text"
+                data-height="none"
+                data-whitespace="nowrap"
+                data-fontsize="['64','48','36','24']"
+                data-width="none"
+                data-hoffset="['0','15','15','15']"
+                data-voffset="['250','200','150','90']"
+                data-x="['left','left','left','left']"
+                data-y="['top','top','top','top']"
+                data-textalign="['top','top','top','top']"
+                data-frames='[{"from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1500,"to":"o:1;","delay":1000,"ease":"Power3.easeInOut"},{"delay":"wait","speed":1000,"to":"auto:auto;","mask":"x:0;y:0;s:inherit;e:inherit;","ease":"Power3.easeInOut"}]'
+                style="z-index: 7; white-space: nowrap;text-transform:left;">
+                  <h2>Parsim <span class="theme_color">NET</span> <br>On-line парсер каталогов</h2>
+                </div>
+                
+                <div class="tp-caption" 
+                data-paddingbottom="[0,0,0,0]"
+                data-paddingleft="[0,0,0,0]"
+                data-paddingright="[0,0,0,0]"
+                data-paddingtop="[0,0,0,0]"
+                data-responsive_offset="on"
+                data-type="text"
+                data-height="none"
+                data-whitespace="normal"
+                data-width="['670','670','600','420']"
+                data-hoffset="['0','15','15','15']"
+                data-voffset="['420','370','300','200']"
+                data-x="['left','left','left','left']"
+                data-y="['top','top','top','top']"
+                data-textalign="['top','top','top','top']"
+                data-frames='[{"from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1500,"to":"o:1;","delay":1000,"ease":"Power3.easeInOut"},{"delay":"wait","speed":1000,"to":"auto:auto;","mask":"x:0;y:0;s:inherit;e:inherit;","ease":"Power3.easeInOut"}]'
+                style="z-index: 7; white-space: nowrap;text-transform:left;">
+                  <div class="text">
+                    Продвинутый онлайн парсером, который поможет Вам автоматизировать рутинные операции, повысить осведомленность о ценах конкурентов, позицииях запросов в поисковой выдаче.
+                    </div>
+                </div>
+                
+                <div class="tp-caption tp-resizeme" 
+                data-paddingbottom="[0,0,0,0]"
+                data-paddingleft="[0,0,0,0]"
+                data-paddingright="[0,0,0,0]"
+                data-paddingtop="[0,0,0,0]"
+                data-responsive_offset="on"
+                data-type="text"
+                data-height="none"
+                data-whitespace="nowrap"
+                data-width="none"
+                data-hoffset="['0','15','15','15']"
+                data-voffset="['510','460','390','350']"
+                data-x="['left','left','left','left']"
+                data-y="['top','top','top','top']"
+                data-textalign="['top','top','top','top']"
+                data-frames='[{"from":"x:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1500,"to":"o:1;","delay":1000,"ease":"Power3.easeInOut"},{"delay":"wait","speed":1000,"to":"auto:auto;","mask":"x:0;y:0;s:inherit;e:inherit;","ease":"Power3.easeInOut"}]'
+                style="z-index: 7; white-space: nowrap;text-transform:left;">
+                  <a href="/site/page?view=about" class="theme-btn btn-style-one"><?= Yii::t('app','Learn More'); ?> <span class="icon fa fa-long-arrow-right"></span></a>
+                </div>
+                
+                <div class="tp-caption tp-resizeme ipad-hidden" 
+                data-paddingbottom="[0,0,0,0]"
+                data-paddingleft="[0,0,0,0]"
+                data-paddingright="[0,0,0,0]"
+                data-paddingtop="[0,0,0,0]"
+                data-responsive_offset="on"
+                data-type="shape"
+                data-height="none"
+                data-whitespace="nowrap"
+                data-width="none"
+                data-hoffset="['0','15','15','15']"
+                data-voffset="['90','0','0','0']"
+                data-x="['right','right','right','right']"
+                data-y="['bottom','bottom','bottom','bottom']"
+                data-textalign="['top','top','top','top']"
+                data-frames='[{"from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1500,"to":"o:1;","delay":2000,"ease":"Power3.easeInOut"},{"delay":"wait","speed":3000,"to":"auto:auto;","mask":"x:0;y:0;s:inherit;e:inherit;","ease":"Power3.easeInOut"}]'
+                style="z-index: 7; white-space: nowrap;text-transform:left;">
+                  <figure class="content-image"><img src="/images/main-slider/content-image-1.png" alt=""></figure>
+                </div>
+                
+                <div class="tp-caption tp-shape tp-shapewrapper tp-resizeme big-ipad-hidden" 
+                data-basealign="slide"
+                data-paddingbottom="[0,0,0,0]"
+                data-paddingleft="[0,0,0,0]"
+                data-paddingright="[0,0,0,0]"
+                data-paddingtop="[0,0,0,0]"
+                data-responsive_offset="on"
+                data-type="shape"
+                data-height="auto"
+                data-whitespace="nowrap"
+                data-width="none"
+                data-hoffset="['0','0','0','0']"
+                data-voffset="['0','0','0','0']"
+                data-x="['right','right','right','right']"
+                data-y="['bottom','bottom','bottom','bottom']"
+                data-frames='[{"from":"x:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1000,"to":"o:1;","delay":1000,"ease":"Power3.easeInOut"},{"delay":"wait","speed":1000,"to":"auto:auto;","mask":"x:0;y:0;s:inherit;e:inherit;","ease":"Power3.easeInOut"}]'
+                style="z-index: 7; white-space: nowrap;text-transform:left;">
+                  <figure class="content-image"><img src="/images/main-slider/content-image-2.png" alt=""></figure>
+                </div>
+                
+                
+                <div class="tp-caption tp-resizeme big-ipad-hidden" 
+                data-paddingbottom="[0,0,0,0]"
+                data-paddingleft="[0,0,0,0]"
+                data-paddingright="[0,0,0,0]"
+                data-paddingtop="[0,0,0,0]"
+                data-responsive_offset="on"
+                data-type="shape"
+                data-height="none"
+                data-whitespace="nowrap"
+                data-width="none"
+                data-hoffset="['-200','-200','-200','-200']"
+                data-voffset="['0','0','0','0']"
+                data-x="['left','left','left','left']"
+                data-y="['bottom','bottom','bottom','bottom']"
+                data-textalign="['top','top','top','top']"
+                data-frames='[{"from":"y:[0%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":0,"to":"o:1;","delay":0,"ease":"Power3.easeInOut"},{"delay":"wait","speed":3000,"to":"auto:auto;","mask":"x:0;y:0;s:inherit;e:inherit;","ease":"Power3.easeInOut"}]'
+                style="z-index: 7; white-space: nowrap;text-transform:left;">
+                  <figure class="content-image"><img src="/images/main-slider/content-arrow-2.png" alt=""></figure>
+                </div>
+                
+                <div class="tp-caption tp-resizeme big-ipad-hidden" 
+                data-paddingbottom="[0,0,0,0]"
+                data-paddingleft="[0,0,0,0]"
+                data-paddingright="[0,0,0,0]"
+                data-paddingtop="[0,0,0,0]"
+                data-responsive_offset="on"
+                data-type="shape"
+                data-height="none"
+                data-whitespace="nowrap"
+                data-width="none"
+                data-hoffset="['500','500','400','250']"
+                data-voffset="['-150','-150','-150','-150']"
+                data-x="['left','left','left','left']"
+                data-y="['middle','middle','middle','middle']"
+                data-textalign="['top','top','top','top']"
+                data-frames='[{"from":"y:[0%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":0,"to":"o:1;","delay":3000,"ease":"Power3.easeInOut"},{"delay":"wait","speed":0,"to":"auto:auto;","mask":"x:0;y:0;s:inherit;e:inherit;","ease":"Power3.easeInOut"}]'
+                style="z-index: 7; white-space: nowrap;text-transform:left;">
+                  <figure class="content-image"><img src="/images/main-slider/content-arrow-1.png" alt=""></figure>
+                </div>
+                
+                </li>
+            </ul>
+            
+        </div>
+    </div>
+</section>
+<!--End Main Slider-->
 
 <!--Services Section-->
 <section class="services-section" >
