5c814a5674119e677156b60eb5e6349fae756707
PTimofeev <info@ptimofeev.com>
2018-01-12 16:08:11 +0300

    #

--- frontend/views/site/_tarif.php
+++ frontend/views/site/_tarif.php
@@ -4,7 +4,7 @@
         <div class="price-header">
             <div class="content">
                 <div class="plan-title"><?= $model->name; ?></div>
-                <span class="price"><span class="dollar-sign">$</span><span class="total-amount"> 9 </span> <sup>.99</sup> <sub>/ month</sub></span>
+                <span class="price"><span class="dollar-sign">₽</span><span class="total-amount"> <?= $model->price; ?> </span> <sup>.99</sup> <sub>/ month</sub></span>
             </div>
         </div>
         <div class="lower-content">
