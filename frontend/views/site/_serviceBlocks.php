78c6f835b98a6038504be55371e6e565f201e6b6
PTimofeev <info@ptimofeev.com>
2018-01-12 15:25:34 +0300

    #

--- frontend/views/site/_serviceBlocks.php
+++ frontend/views/site/_serviceBlocks.php
@@ -2,7 +2,7 @@
 <div class="services-title">
   <div class="row clearfix">
       <div class="column col-md-6 col-sm-12 col-xs-12">
-          <h2>Parsim <span class="theme_color">NET</span> <br> просто, удобно, надежно</h2>
+          <h2>Parsim <span class="theme_color">NET</span> <br> просто/удобно/надежно</h2>
         </div>
         <div class="column col-md-6 col-sm-12 col-xs-12">
             <div class="text">
