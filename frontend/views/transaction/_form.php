b6afd8ac510be6cbaeced59935ee89c9b6a62114
PTimofeev <info@ptimofeev.com>
2018-01-15 18:14:23 +0300

    #

--- frontend/views/transaction/_form.php
+++ frontend/views/transaction/_form.php
@@ -36,10 +36,8 @@ FormDynamicAsset::register($this);
         ?>
         
         <?= $form->field($model, 'amount')->textInput(['placeholder'=>'Сумма оплаты в рублях','name'=>'amount'])->label(false); ?>
-        <?= $form->field($model, 'tarif_id')->hiddenInput(['name'=>'tarif_id'])->label(false) ?>
-        <?= $form->field($model, 'price')->hiddenInput(['id'=>'order-price','name'=>'price'])->label(false) ?>
         
-
+        
         <div class="form-group">
             <?= Html::button('Оплатить', ['class' => 'btn-block theme-btn btn-style-two','id'=>'btn-submit']) ?>
         </div>
