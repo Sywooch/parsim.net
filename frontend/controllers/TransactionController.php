bcf000b35a9c79f84605777dfb06af61bd1a3e9c
PTimofeev <info@ptimofeev.com>
2018-01-23 16:47:08 +0300

    #

--- frontend/controllers/TransactionController.php
+++ frontend/controllers/TransactionController.php
@@ -95,9 +95,9 @@ class TransactionController extends Controller
                             }
                             
                             if($currentOrder && !$currentOrder->isPaid){
-                                $currentOrder->pay();
-                                //$currentOrder->status=1;
-                                //$currentOrder->save();
+                                //$currentOrder->pay();
+                                $currentOrder->status=1;
+                                $currentOrder->save();
 
                             }
                             return true;
