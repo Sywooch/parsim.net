752dbc19a94ed6757b9f8ae3362d3e14f77808f4
PTimofeev <info@ptimofeev.com>
2018-01-29 17:01:33 +0300

    #

--- frontend/views/request/_view.php
+++ frontend/views/request/_view.php
@@ -1,5 +1,5 @@
 <tr>
-    <td><a href="<?= $model->viewUrl; ?>"><?= $model->alias; ?></a></td></td>
+    <td><a href="<?= $model->getUrl('frontend','view'); ?>"><?= $model->alias; ?></a></td></td>
     <td><?= $model->request_url; ?></td>
     <td><a href="<?= $model->viewUrl; ?>"><?= $model->responseCount; ?></a></td>
     <td><?= $model->statusName; ?></td>
