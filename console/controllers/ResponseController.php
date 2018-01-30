818afc2328dcf48babe1814f6e6dda370e505be3
PTimofeev <info@ptimofeev.com>
2018-01-30 20:56:07 +0300

    #

--- console/controllers/ResponseController.php
+++ console/controllers/ResponseController.php
@@ -58,55 +58,15 @@ class ResponseController extends Controller
             }
 
         }
-
-
-
-        /*
-
-        //Создаю новые ответы для запросов в статусе READY
-        //Это запросы для которых настало время обновить информацию
-        $requests=Request::find()->where(['status'=>[Request::STATUS_READY]])->all();
         
-        foreach ($requests as $key => $request) {
-            //Создаю новый ответ
-            $request->addResponse();
-        }
-
-        //Загрузка контента htmlClient
-        $responses=Response::find()->where(['status'=>Response::STATUS_READY])->all();
-        foreach ($responses as $key => $response){
-            
-            $request=$response->request;
-            $content_path=$response->contentPath;
-
-            //$this->stdout('Path: '.$content_path.PHP_EOL);
-
-            if($response->loader->type==HttpLoader::TYPE_HTTP){
-                //$response->status=Response::STATUS_LOADING;    
-                //$response->save();
-
-                $loader=new HttpLoader();
-
-                if($loader->loadContent($request->request_url,$content_path)){
-                    $response->regEventContentLoad();
-                }else{
-                    //Регистрирую ошибку загрузки контента
-                    //$response->regError(Response::STATUS_LOADING_ERROR,'Ошибка загрузки контента');
-                }
-                
-            }
-        }
-
 
         //Парсинг загруженного контента
         $responses=Response::find()->where(['status'=>Response::STATUS_LOADING_SUCCESS])->all();
         foreach ($responses as $key => $response){
-
             
             $parser = BaseParser::initParser($response);
-
+            
             if($data=$parser->run()){
-                
                 $response->regData($data);
 
             }else{
@@ -114,13 +74,14 @@ class ResponseController extends Controller
 
                 //Блокирую последующую обработку ответа
                 //и сторнирую оплату (если была)
-                $response->rollBack(Response::STATUS_PARSING_ERROR);
                 
-                $response->regError(Response::STATUS_PARSING_ERROR,json_encode($info));
+                //$response->rollBack(Response::STATUS_PARSING_ERROR);
+                
+                //$response->regError(Response::STATUS_PARSING_ERROR,json_encode($info));
 
             }
         }
-        */
+        
         
     }
 
