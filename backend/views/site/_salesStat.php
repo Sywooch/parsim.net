4be43961698f529687a0e8f6f1a2cb3c8a85599c
PTimofeev <info@ptimofeev.com>
2018-01-30 14:41:30 +0300

    #

--- backend/views/site/_salesStat.php
+++ backend/views/site/_salesStat.php
@@ -2,7 +2,7 @@
   <div class="panel-heading">
     <h6 class="panel-title">Входящие платежи</h6>
     <div class="heading-elements">
-      <span class="heading-text">Ammount: <span class="text-bold text-danger-600 position-right">$4,378</span></span>
+      <span class="heading-text">Итого: <span class="text-bold text-danger-600 position-right">$4,378</span></span>
       <button type="button" class="btn btn-link daterange-ranges heading-btn text-semibold">
         <i class="icon-calendar3 position-left"></i> <span></span> <b class="caret"></b>
       </button>
