5295f7e033c1fd540f71560ff1fdec2dd3cf971e
PTimofeev <info@ptimofeev.com>
2018-02-02 19:53:46 +0300

    #

--- frontend/views/request/_view.php
+++ frontend/views/request/_view.php
@@ -1,7 +1,7 @@
 <tr>
     <td><a href="<?= $model->getUrl('frontend','view'); ?>"><?= $model->alias; ?></a></td></td>
     <td><?= $model->request_url; ?></td>
-    <td><a href="<?= $model->getUrl('frontend','responses'); ?>"><?= $model->responseCount; ?></a></td>
+    <td><a href="<?= $model->getUrl('frontend','view'); ?>"><?= $model->responseCount; ?></a></td>
     <td><?= $model->statusName; ?></td>
     <td><?= Yii::$app->formatter->asDate($model->created_at); ?></td>
 </tr>
